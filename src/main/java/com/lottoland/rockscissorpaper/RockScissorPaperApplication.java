0f2baeff2b3e5eed36e4e582ba3cb46894e11d5f
Iván <i.yuguerosvi2@gmail.com>
2021-11-08 22:17:00 +0100

    build: folder structure created

---
+++ src/main/java/com/lottoland/rockscissorpaper/RockScissorPaperApplication.java
@@ -0,0 +1,13 @@
+package com.lottoland.rockscissorpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockScissorPaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockScissorPaperApplication.class, args);
+	}
+
+}
